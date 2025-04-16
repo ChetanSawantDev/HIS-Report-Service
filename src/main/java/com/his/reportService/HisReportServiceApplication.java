d990a32f0235df1af7312a406fedf745d9553ba3
ChetanSawant <chetansawant.dev@gmail.com>
2025-04-16 07:44:27 +0530

    First Commit

---
+++ src/main/java/com/his/reportService/HisReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.his.reportService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisReportServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/his/reportService/HospitalInformationSystemReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.his.reportService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalInformationSystemReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
